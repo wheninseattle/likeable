8dfc6703c7a6e68985a954d767460104a3e0ea3c
Weston Norwood <weston.norwood@gmail.com>
2023-06-24 15:32:25 -0700

    fix: Exchange custom model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -5,7 +5,7 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
 const Model = () => {
   const objRef = useRef();
-  const modelDisplay = useLoader(OBJLoader, "../../Japanese_Temple.obj");
+  const modelDisplay = useLoader(OBJLoader, "../../Test1.obj");
     
   useFrame(() => {
       objRef.current.rotation.x += 0.01;
