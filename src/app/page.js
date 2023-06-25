2ff63081052fbf5794a5d024c63fd31d320f8796
Weston Norwood <weston.norwood@gmail.com>
2023-06-25 09:49:45 -0700

    fix: Splash screen responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 'use client'
 import { useState } from 'react';
-import { Box, Container, ThemeProvider } from '@mui/material'
+import { Box, Container, ThemeProvider, useMediaQuery } from '@mui/material'
 import theme from '@/styles/theme'
-import { Button, Stack, Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import IconBackgroundIcons from '@/components/icons/IconBackgroundIcons'
 
 import IconStart from '@/components/icons/IconStart'
@@ -17,16 +17,18 @@ export default function Home() {
   setTimeout(() => { setFadeIn(true) }, 2200);
   setTimeout(() => { setHideSplash(true) }, 3000);
 
+  const notMobile = useMediaQuery(theme.breakpoints.up('sm'));
+
   return (
     <ThemeProvider theme={theme} >
-      <Container maxWidth="sm" sx={{ position: 'absolute', height: '100vh', background: theme.palette.primary.mainGradient, display: hideSplash ? 'none' : 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden', opacity: fadeIn ? 0 : 1, transition: 'opacity 1s ease-out', zIndex: 1}}>
+      <Container maxWidth= {false} sx={{ position: 'absolute', height: '100vh', background: theme.palette.primary.mainGradient, display: hideSplash ? 'none' : 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden', opacity: fadeIn ? 0 : 1, transition: 'opacity 1s ease-out', zIndex: 1}}>
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', }}>
           <IconHeart />
           <Typography variant="h1" gutterBottom sx={{ color: 'white' }}>Likeable</Typography>
           <Typography variant="body1" gutterBottom sx={{ color: 'white' }}>Swiping right on great design</Typography>
         </Box>
       </Container>
-      <Container maxWidth="sm" sx={{ position: 'relative', height: '100vh', background: theme.palette.primary.mainGradient, display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden', zIndex:0}}>
+      <Container maxWidth= {false} sx={{ position: 'relative', height: '100vh', background: theme.palette.primary.mainGradient, display:'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden', zIndex:0}}>
         <Button href='/swipe' sx={{ position: 'absolute',right: 0, padding: 0 }}>
           <IconStart />
         </Button>
