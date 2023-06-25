0905c46c45c11277d677d257dc04a0bd84c3efbe
FX Wood <fxwood2@gmail.com>
2023-06-24 20:19:56 -0700

    stop seed files from running during build

--- prisma/query.mjs
+++ prisma/query.mjs
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
 
-const main = async () => {
-    const meshes = await prisma.mesh.findMany()
-    console.log(meshes[0])
+if (process.env.SEED_MODE !== 'seed') {
+    const main = async () => {
+        const meshes = await prisma.mesh.findMany()
+        console.log(meshes[0])
+    }
+    
+    main()
 }
-
-main()
--- prisma/updateMeshes.mjs
+++ prisma/updateMeshes.mjs
@@ -1,24 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 import fs from 'fs/promises';
 
-const prisma = new PrismaClient()
+if (process.env.SEED_MODE !== 'seed') {
+    const prisma = new PrismaClient()
 
-const main = async () => {
-    const meshes = await prisma.mesh.findMany()
+    const main = async () => {
+        const meshes = await prisma.mesh.findMany()
 
-    for (let mesh of meshes) {
-        const meshFileBuffer = await fs.readFile(`./data/mesh-files/${mesh.id}.obj`)
-        const meshFileString = String(meshFileBuffer)
-        const res = await prisma.mesh.update({
-            where: {
-                id: mesh.id
-            },
-            data: {
-                data: meshFileString.replaceAll('\r\n','\n')
-            }
-        })
-        console.log(res)
+        for (let mesh of meshes) {
+            const meshFileBuffer = await fs.readFile(`./data/mesh-files/${mesh.id}.obj`)
+            const meshFileString = String(meshFileBuffer)
+            const res = await prisma.mesh.update({
+                where: {
+                    id: mesh.id
+                },
+                data: {
+                    data: meshFileString.replaceAll('\r\n','\n')
+                }
+            })
+            console.log(res)
+        }
     }
-}
 
-main()
+    main()
+}
