de89aa1948596d1fd5f2f16fa45cb095e0cf4072
Weston Norwood <weston.norwood@gmail.com>
2023-06-25 08:39:21 -0700

    fix: Hide addressbar ios

--- src/app/swipe/page.jsx
+++ src/app/swipe/page.jsx
@@ -5,81 +5,74 @@ import Model from "@/components/Model";
 import theme from "@/styles/theme";
 import BottomDrawer from "@/components/swipe/BottomDrawer";
 import SwipeButtons from "@/components/swipe/SwipeButtons";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-const MESH_SERVER = 'https://mesh-server-880e3482d864.herokuapp.com'
+const MESH_SERVER = "https://mesh-server-880e3482d864.herokuapp.com";
 // const MESH_SERVER = 'http://localhost:3000'
 
-const deg2rad = degrees => degrees * (Math.PI / 180);
-
-const Scene = () => {
-  useThree(({ camera }) => {
-    // camera.position.set(0, 0, 0);
-    camera.rotation.set(deg2rad(60), deg2rad(90), deg2rad(0));
-    // camera.lookAt(0, 0, 0);
-  })
-}
-
 const getMesh = async () => {
-  const res = await fetch(`${MESH_SERVER}/mesh`)
-  const json = await res.json()
-  console.log('json', json)
-  return json
-}
+  const res = await fetch(`${MESH_SERVER}/mesh`);
+  const json = await res.json();
+  console.log("json", json);
+  return json;
+};
 
 const Swipe = () => {
-  const [currentMesh, setCurrentMesh] = useState(undefined)
+  const [currentMesh, setCurrentMesh] = useState(undefined);
+
+  const scrollInto = useRef(null);
 
   const setMesh = (mesh) => {
-    console.log('setting mesh in rando', mesh)
-    setCurrentMesh(mesh)
-  }
+    console.log("setting mesh in rando", mesh);
+    setCurrentMesh(mesh);
+  };
 
   const handleLeft = async () => {
-    console.log('left')
+    console.log("left");
     const body = {
       meshId: currentMesh.id,
-      liked: false
-    }
-    console.log(body)
+      liked: false,
+    };
+    console.log(body);
     const res = await fetch(`${MESH_SERVER}/cluster`, {
       method: "POST",
-      body: JSON.stringify(body)
-    })
-    const nextMesh = await getMesh()
-    console.log('nextMesh', nextMesh)
-    // setMesh(nextMesh)
-    setCurrentMesh(nextMesh)
-  }
+      body: JSON.stringify(body),
+    });
+    const nextMesh = await getMesh();
+    console.log("nextMesh", nextMesh);
+    setMesh(nextMesh);
+    // setCurrentMesh(nextMesh)
+  };
 
   const handleRight = async () => {
-    console.log('right')
+    console.log("right");
     const body = {
       meshId: currentMesh.id,
-      liked: true
-    }
-    console.log(body)
+      liked: true,
+    };
+    console.log(body);
     const res = await fetch(`${MESH_SERVER}/cluster`, {
       method: "POST",
-      body: JSON.stringify(body)
-    })
-    const nextMesh = await getMesh()
-    console.log('nextMesh', nextMesh)
-    setMesh(nextMesh)
-  }
+      body: JSON.stringify(body),
+    });
+    const nextMesh = await getMesh();
+    console.log("nextMesh", nextMesh);
+    setMesh(nextMesh);
+  };
 
   useEffect(() => {
+    scrollInto.current.scrollIntoView({ behavior: "smooth" });
     const fetchData = async () => {
       try {
-        const mesh = await getMesh()
-        console.log('useEffect setting mesh', mesh)
-        setMesh(mesh)
-      } catch (error){
-        console.log('error', error)
+        const mesh = await getMesh();
+        console.log("useEffect setting mesh", mesh);
+        setMesh(mesh);
+      } catch (error) {
+        console.log("error", error);
       }
-    }
-    fetchData()
-  }, [])
+    };
+    fetchData();
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -107,21 +100,19 @@ const Swipe = () => {
             width: "100%",
           }}
         >
-          <SwipeButtons variant={"left"} onClick={handleLeft}/>
-          <SwipeButtons variant={"right"} onClick={handleRight}/>
+          <SwipeButtons variant={"left"} onClick={handleLeft} />
+          <SwipeButtons variant={"right"} onClick={handleRight} />
         </Box>
-        <Box sx={{ width: "100%", height: "100vh", paddingX:'3rem'}}>
-          <Canvas camera={{fov: 35, zoom: 1.3}}>
+        <Box sx={{ width: "100%", height: "100vh", paddingX: "3rem" }}>
+          <Canvas camera={{ fov: 35, zoom: 1.3 }}>
             <ambientLight intensity={0.1} />
             <directionalLight color="white" position={[0, 0, 5]} />
             <directionalLight color="white" position={[0, 5, 0]} />
-          {currentMesh && <Model mesh={currentMesh} /> }
+            {currentMesh && <Model mesh={currentMesh} />}
           </Canvas>
         </Box>
-        {/* TODO: Limit to width of container and finish styling */}
-        (
-          {currentMesh && <BottomDrawer mesh={currentMesh}/>}
-          )
+        {currentMesh && <BottomDrawer mesh={currentMesh} />}
+        <div ref={scrollInto} />
       </Container>
     </ThemeProvider>
   );
