cb7b2f60cd404c367408cbe43e3f50d0d06fc16a
FX Wood <fxwood2@gmail.com>
2023-06-24 20:25:39 -0700

    fix console.log

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import prisma from '@/lib/prisma'
 
 const dashboard = async () => {
-  const meshes = await prisma.mesh.findMany()
-  console.log(meshes)
   return (
     <div>dashboard</div>
   )
